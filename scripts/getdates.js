dc73a1d14bc5f754b4b22f8806a0bb1f5f010d9b
sariahsanmartinerazo <sanmartinsariah@gmail.com>
2026-07-12 10:05:18 -0600

    save revision

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,3 +1,3 @@
 const today = new Date();
-document.getElementById("currentyear").textContent = today.getFullyear();
+document.getElementById("currentyear").textContent = today.getFullYear();
 document.getElementById("lastModified").textContent = document.lastModified;
