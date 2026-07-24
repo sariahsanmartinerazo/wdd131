4da23de6ab9c0220eada91b2cc005193173cfd3d
sariahsanmartinerazo <sanmartinsariah@gmail.com>
2026-07-23 18:39:56 -0600

    save

---
+++ scripts/place.js
@@ -0,0 +1,22 @@
+const currentYear = document.querySelector("#currentyear");
+const lastModified = document.querySelector("#lastModified");
+const windChillDisplay = document.querySelector("#wind-chill");
+
+const temperature = 8;
+const windSpeed = 12;
+
+currentYear.textContent = new Date().getFullYear();
+lastModified.textContent = `Last Modification: ${document.lastModified}`;
+
+const calculateWindChill = (temperature, windSpeed) =>
+    13.12 +
+    0.6215 * temperature -
+    11.37 * windSpeed ** 0.16 +
+    0.3965 * temperature * windSpeed ** 0.16;
+
+if (temperature <= 10 && windSpeed > 4.8) {
+    windChillDisplay.textContent =
+        `${calculateWindChill(temperature, windSpeed).toFixed(1)} °C`;
+} else {
+    windChillDisplay.textContent = "N/A";
+}
