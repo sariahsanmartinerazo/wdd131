f1a4acef8514234babf1a2568b71de37cc61d55f
sariahsanmartinerazo <sanmartinsariah@gmail.com>
2026-08-05 08:47:09 -0600

    save

--- week05/bom.js
+++ week05/bom.js
@@ -7,3 +7,26 @@ let chaptersArray = getChapterList() || [];
 chaptersArray.forEach((chapter) => {
     displayList(chapter);
 });
+
+function displayList(item) {
+    const li = document.createElement("li");
+    const deleteButton = document.createElement("button");
+
+    li.textContent = item;
+
+    deleteButton.textContent = "❌";
+    deleteButton.setAttribute("aria-label", `Remove ${item}`);
+
+    li.append(deleteButton);
+    list.append(li);
+}
+
+button.addEventListener("click", () => { 
+    if (input.ariaValueMax.trim() !== "") {
+        displayList(input.value);
+        chaptersArray.push(input.value);
+        setChapterList();
+        input.value = "";
+        input.focus();
+    }
+});
