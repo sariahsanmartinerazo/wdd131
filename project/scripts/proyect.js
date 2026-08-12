17f3618068bb0df0225e651912de2417edbc2822
sariahsanmartinerazo <sanmartinsariah@gmail.com>
2026-08-11 19:30:39 -0600

    s

--- project/scripts/proyect.js
+++ project/scripts/proyect.js
@@ -3,3 +3,22 @@ const lastModified = document.querySelector("#lastModified");
 
 currentYear.textContent = `${new Date().getFullYear()}`;
 lastModified.textContent = `Last Modification: ${document.lastModified}`;
+
+const destinations = [
+    {
+        name: "New Zealand",
+        description: "Explore mountains, lakes, and outdoor adventures."
+    },
+    {
+        name: "Japan",
+        description: "Discover ancient traditions and modern cities."
+    },
+    {
+        name: "Australia",
+        description: "Experience beaches, wildlife, and natural landscapes."
+    },
+    {
+        name: "Brazil",
+        description: "Discover vibrant culture and natural beauty."
+    }
+];
