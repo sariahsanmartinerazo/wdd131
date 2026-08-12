be3baed99cdc729547a34e7a46080fdb474d8d02
sariahsanmartinerazo <sanmartinsariah@gmail.com>
2026-08-12 16:10:08 -0600

    s

--- project/scripts/project.js
+++ project/scripts/project.js
@@ -1,9 +1,13 @@
 const currentYear = document.querySelector("#currentyear");
 const lastModified = document.querySelector("#lastModified");
 
-currentYear.textContent = new Date().getFullYear();
-lastModified.textContent = `Last Modification: ${document.lastModified}`;
+if (currentYear) {
+    currentYear.textContent = new Date().getFullYear();
+}
 
+if (lastModified) {
+    lastModified.textContent = `Last Modification: ${document.lastModified}`;
+}
 const destinations = [
     {
         id: "new-zealand",
